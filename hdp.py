07169df902247a29bc5cb84b0b8f99d2ef3f2116
KIRANKALLA <kallakiran1974@gmail.com>
2022-08-27 09:31:56 +0530

    Add files via upload

---
+++ hdp.py
@@ -0,0 +1,23 @@
+import pickle
+import streamlit as st
+pickle_in=open('Classifier.pkl','rb')
+clf=pickle.load(pickle_in)
+
+a=st.number_input('AGE')
+b=st.number_input('CIGARETTES PER DAY')
+c=st.number_input('TOTAL CHOLESTROL')
+d=st.number_input('SYSTOLIC BP')
+e=st.number_input('DISTOLIC BP')
+f=st.number_input('BMI')
+g=st.number_input('HEART RATE')
+h=st.number_input('GLUCOSE')
+
+
+if st.button('PREDICT'):
+    result=clf.predict([[a,b,c,d,e,f,g,h]]).squeeze()
+    if result==0:
+        st.success('YOUR RISK IS LOW')
+    else:
+        st.success('YOUR RISK IS HIGH')
+
+  
