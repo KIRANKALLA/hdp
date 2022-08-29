04392e77eb928e6935ef193d5862c7673f5bfaf3
KIRANKALLA <kallakiran1974@gmail.com>
2022-08-29 14:39:38 +0530

    Update hdp.py

--- hdp.py
+++ hdp.py
@@ -3,14 +3,14 @@ import streamlit as st
 pickle_in=open('hdp.pkl','rb')
 clf=pickle.load(pickle_in)
 
-a = st.slider('How old are you?', 0, 130, 25)
-b = st.slider('How many cigartees you smoke daily?', 0,20,1)
-c = st.slider('Total Cholestrol?', 50, 250, 10)
-d = st.slider('Systolic BP ?', 70, 230, 10)
-e = st.slider('Diastolic BP?', 60, 120, 5)
-f = st.slider('BMI?', 15,30,1)
-g = st.slider('Heart Rate?', 50, 130, 5)
-h = st.slider('Glucose levels?',100, 250, 15)
+a = st.slider('How old are you?', 1, 130, 25)
+b = st.slider('How many cigartees you smoke daily?', 0,20,10)
+c = st.slider('Total Cholestrol?', 50, 250, 50)
+d = st.slider('Systolic BP ?', 70, 230, 80)
+e = st.slider('Diastolic BP?', 60, 120, 65)
+f = st.slider('BMI?', 15,30,16)
+g = st.slider('Heart Rate?', 50, 130, 55)
+h = st.slider('Glucose levels?',100, 250, 105)
 
 
 
