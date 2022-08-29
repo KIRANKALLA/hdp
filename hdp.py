d7f1b18a8d93878bdbe7388b32439959c739eebe
KIRANKALLA <kallakiran1974@gmail.com>
2022-08-29 14:37:06 +0530

    Update hdp.py

--- hdp.py
+++ hdp.py
@@ -3,14 +3,25 @@ import streamlit as st
 pickle_in=open('hdp.pkl','rb')
 clf=pickle.load(pickle_in)
 
-a=st.number_input('AGE')
+a = st.slider('How old are you?', 0, 130, 25)
+b = st.slider('How many cigartees you smoke daily?', 0,20,1)
+c = st.slider('Total Cholestrol?', 50, 250, 10)
+d = st.slider('Systolic BP ?', 70, 230, 10)
+e = st.slider('Diastolic BP?', 60, 120, 5)
+f = st.slider('BMI?', 15,30,1)
+g = st.slider('Heart Rate?', 50, 130, 5)
+h = st.slider('Glucose levels?',100, 250, 15)
+
+
+
+'''a=st.number_input('AGE')
 b=st.number_input('CIGARETTES PER DAY')
 c=st.number_input('TOTAL CHOLESTROL')
 d=st.number_input('SYSTOLIC BP')
 e=st.number_input('DISTOLIC BP')
 f=st.number_input('BMI')
 g=st.number_input('HEART RATE')
-h=st.number_input('GLUCOSE')
+h=st.number_input('GLUCOSE')'''
 
 
 if st.button('PREDICT'):
